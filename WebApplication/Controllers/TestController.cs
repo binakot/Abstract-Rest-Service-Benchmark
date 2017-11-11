ac1f7b91dd1bf06b0420786f9afdf332adf929d3
Ivan Muratov <binakot@gmail.com>
2017-11-11 22:50:23 +0300

    Refactoring code. Added test rest api controller with endpoint /api/test.

---
+++ WebApplication/Controllers/TestController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication.Controllers
+{
+    [Route("api/[controller]")]
+    public class TestController : Controller
+    {
+        // GET api/test
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new[] { "test" };
+        }
+    }
+}
