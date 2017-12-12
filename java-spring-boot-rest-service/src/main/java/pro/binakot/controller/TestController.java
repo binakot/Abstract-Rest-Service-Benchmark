e759a8345175db62e7e9a26d400b1e3d282b3ac1
Ivan Muratov <binakot@gmail.com>
2017-12-12 21:06:12 +0300

    Added JSON serialization for Java service. Remove and ignore publish fodler for .NET server.

--- java-spring-boot-rest-service/src/main/java/pro/binakot/controller/TestController.java
+++ java-spring-boot-rest-service/src/main/java/pro/binakot/controller/TestController.java
@@ -1,5 +1,6 @@
 package pro.binakot.controller;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/test")
 public class TestController {
 
-    @RequestMapping(method = RequestMethod.GET)
+    @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public String test() {
         return "Hello, World!";
     }
