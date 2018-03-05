61635e00240656ffa59876c9c1f803f19a88ac13
wilix-lead <lead@w.wilix.ru>
2018-03-05 17:25:25 +0300

    Add Cluster support to Node.js (Express)

--- node.js/index.js
+++ node.js/index.js
@@ -1,15 +1,24 @@
 'use strict';
+const cluster = require('cluster');
 const http = require('http');
 const url = require('url');
 const PORT = process.env.PORT || 8080;
+const numCPUs = require('os').cpus().length;
 
-http.createServer(function (req, res) {
-  let rUrl = url.parse(req.url);
-  if (rUrl.path == '/api/test') {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello, World!')
-  } else {
-    res.writeHead(404, {'Content-Type': 'text/plain'});
-    res.end('Not found');
+if (cluster.isMaster) {
+  // Fork workers.
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
   }
-}).listen(PORT);
+} else {
+  http.createServer(function(req, res) {
+    let rUrl = url.parse(req.url);
+    if (rUrl.path == '/api/test') {
+      res.writeHead(200, {'Content-Type': 'text/plain'});
+      res.end('Hello, World!')
+    } else {
+      res.writeHead(404, {'Content-Type': 'text/plain'});
+      res.end('Not found');
+    }
+  }).listen(PORT);
+}
