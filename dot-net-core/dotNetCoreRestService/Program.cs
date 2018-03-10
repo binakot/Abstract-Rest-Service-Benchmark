963585e7bfb6474efa9d82f01bf21362b149adc5
Friedrich von Never <friedrich@fornever.me>
2018-03-10 22:24:09 +0700

    Don't use MVC in .NET Core sample

--- dot-net-core/dotNetCoreRestService/Program.cs
+++ dot-net-core/dotNetCoreRestService/Program.cs
@@ -1,5 +1,7 @@
-using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Server.Kestrel.Transport.Abstractions.Internal;
 
 namespace dotNetCoreRestService
 {
@@ -7,12 +9,26 @@ namespace dotNetCoreRestService
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            new WebHostBuilder()
+                .UseKestrel(o =>
+                {
+                    o.ApplicationSchedulingMode = SchedulingMode.Inline;
+                    o.AllowSynchronousIO = false;
+                })
+                .Configure(app => app.Run(c =>
+                {
+                    if (c.Request.Path == "/api/test")
+                    {
+                        return c.Response.WriteAsync("Hello, World!");
+                    }
+                    else
+                    {
+                        c.Response.StatusCode = 404;
+                        return c.Response.WriteAsync("Not Found");
+                    }
+                }))
+                .Build()
+                .Run();
         }
-
-        private static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
-                .Build();
     }
 }
